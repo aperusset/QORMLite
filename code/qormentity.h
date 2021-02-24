aaad9e7c73a973c2a8fd105bbbe71902806ca573
Alexandre Perusset <alexandre.perusset@kudelskisecurity.com>
2021-02-24 08:30:40 +0100

    Make QORMEntity notification methods virtual

--- code/qormentity.h
+++ code/qormentity.h
@@ -15,13 +15,27 @@ class QORMEntity {
 public:
     explicit QORMEntity(const Key &key) : key(key) {}
     QORMEntity(const QORMEntity&) = delete;
+    QORMEntity(QORMEntity&&) = delete;
     QORMEntity& operator=(const QORMEntity&) = delete;
+    QORMEntity& operator=(QORMEntity&&) = delete;
     virtual ~QORMEntity() {}
 
     auto getKey() const -> Key { return this->key; }
     void setKey(const Key &key) { this->key = key; }
 
-    void notifyChange() const {
+    auto isAttached(QORMObserver<Key> &observer) const -> bool {
+        return observers.find(&observer) != observers.end();
+    }
+
+    virtual void attach(QORMObserver<Key> &observer) {
+        observers.insert(&observer);
+    }
+
+    virtual void detach(QORMObserver<Key> &observer) {
+        observers.erase(&observer);
+    }
+
+    virtual void notifyChange() const {
         std::for_each(observers.begin(), observers.end(),
             [this](QORMObserver<Key> *observer) {
                 observer->onChange(this->key);
@@ -29,7 +43,7 @@ public:
         );
     }
 
-    void notifyDelete() const {
+    virtual void notifyDelete() const {
         std::for_each(observers.begin(), observers.end(),
             [this](QORMObserver<Key> *observer) {
                 observer->onDelete(this->key);
@@ -37,27 +51,17 @@ public:
         );
     }
 
-    void attach(QORMObserver<Key> &observer) {
-        observers.insert(&observer);
-    }
-
-    void detach(QORMObserver<Key> &observer) {
-        observers.erase(&observer);
-    }
-
-    auto isAttached(QORMObserver<Key> &observer) const -> bool {
-        return observers.find(&observer) != observers.end();
-    }
-
-    virtual auto exists() const -> bool = 0;
     virtual auto save() -> bool {
         this->notifyChange();
         return true;
     };
+
     virtual auto erase() -> bool {
         this->notifyDelete();
         return true;
     };
+
+    virtual auto exists() const -> bool = 0;
 };
 
 #endif // QORMENTITY_H
