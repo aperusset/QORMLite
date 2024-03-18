ffed71e1e190cab23a66be1de32dd888523a6741
Alexandre Perusset <aperusset@gmail.com>
2024-03-18 16:43:05 +0100

    When notifying an observer, give information about type (#76)
    * When notifying an observer, give information about type
    
    * Return const type_index

--- test/fixture/testobserver.cpp
+++ test/fixture/testobserver.cpp
@@ -3,20 +3,22 @@
 
 TestObserver::TestObserver() {}
 
-void TestObserver::onChange(const int &key) {
-    this->changedKeys.push_back(key);
+void TestObserver::onChange(const int &key, const std::type_index &index) {
+    this->changedKeys.push_back(std::make_pair(key, index));
 }
 
-void TestObserver::onDelete(const int &key) {
-    this->deletedKeys.push_back(key);
+void TestObserver::onDelete(const int &key, const std::type_index &index) {
+    this->deletedKeys.push_back(std::make_pair(key, index));
 }
 
-auto TestObserver::wasChanged(const int key) const -> bool {
-    return QORM::Utils::contains(this->changedKeys, key);
+auto TestObserver::wasChanged(const int key, const std::type_index &index)
+const -> bool {
+    return QORM::Utils::contains(this->changedKeys, std::make_pair(key, index));
 }
 
-auto TestObserver::wasDeleted(const int key) const -> bool {
-    return QORM::Utils::contains(this->deletedKeys, key);
+auto TestObserver::wasDeleted(const int key,  const std::type_index &index)
+const -> bool {
+    return QORM::Utils::contains(this->deletedKeys, std::make_pair(key, index));
 }
 
 void TestObserver::reset() {
