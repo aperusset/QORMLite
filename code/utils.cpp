26887900dc2ac71021ba11446c331e8701fd2602
Alexandre Perusset <aperusset@gmail.com>
2024-06-17 10:08:54 +0200

    Add more getOrThrow* method in utils (#90)

--- code/utils.cpp
+++ code/utils.cpp
@@ -57,11 +57,23 @@ auto QORM::Utils::getBoolOrDefault(const QSqlRecord &record,
                               &QVariant::toBool);
 }
 
+auto QORM::Utils::getBoolOrThrow(const QSqlRecord &record,
+                                 const QString &fieldName) -> bool {
+    return getOrThrow<bool>(record, fieldName, "A valid bool is expected",
+                            &QVariant::toBool);
+}
+
 auto QORM::Utils::getStringOrDefault(const QSqlRecord &record,
                                      const QString &fieldName,
                                      const QString &defaultValue) -> QString {
     return getOrDefault<QString>(record, fieldName, defaultValue,
                                  &QVariant::toString);
+}
+
+auto QORM::Utils::getStringOrThrow(const QSqlRecord &record,
+                                 const QString &fieldName) -> QString {
+    return getOrThrow<QString>(record, fieldName, "A valid QString is expected",
+                               &QVariant::toString);
 }
 
 auto QORM::Utils::getDateOrDefault(const QSqlRecord &record,
@@ -71,12 +83,24 @@ auto QORM::Utils::getDateOrDefault(const QSqlRecord &record,
                                &QVariant::toDate);
 }
 
+auto QORM::Utils::getDateOrThrow(const QSqlRecord &record,
+                                 const QString &fieldName) -> QDate {
+    return getOrThrow<QDate>(record, fieldName, "A valid QDate is expected",
+                             &QVariant::toDate);
+}
+
 auto QORM::Utils::getDateTimeOrDefault(const QSqlRecord &record,
                                        const QString &fieldName,
                                        const QDateTime &defaultValue)
     -> QDateTime {
     return getOrDefault<QDateTime>(record, fieldName, defaultValue,
                                    &QVariant::toDateTime);
+}
+
+auto QORM::Utils::getDateTimeOrThrow(const QSqlRecord &record,
+                                 const QString &fieldName) -> QDateTime {
+    return getOrThrow<QDateTime>(record, fieldName,
+                    "A valid QDateTime is expected", &QVariant::toDateTime);
 }
 
 const auto uIntExtractor = [](const auto &variant) -> uint32_t {
@@ -96,22 +120,37 @@ auto QORM::Utils::getUIntOrThrow(const QSqlRecord &record,
                     "A valid unsigned int is expected", uIntExtractor);
 }
 
+const auto intExtractor = [](const auto &variant) -> uint32_t {
+    return variant.toInt();
+};
+
 auto QORM::Utils::getIntOrDefault(const QSqlRecord &record,
                                   const QString &fieldName,
                                   int32_t defaultValue) -> int32_t {
-    return getOrDefault<int32_t>(record, fieldName, defaultValue,
-                                 [](const auto &variant) -> int32_t {
-                                   return variant.toInt();
-                                 });
+    return getOrDefault<int32_t>(record, fieldName, defaultValue, intExtractor);
 }
+
+auto QORM::Utils::getIntOrThrow(const QSqlRecord &record,
+                                const QString &fieldName) -> int32_t {
+    return getOrThrow<int32_t>(record, fieldName, "A valid int is expected",
+                               intExtractor);
+}
+
+const auto doubleExtractor = [](const auto &variant) -> double {
+    return variant.toDouble();
+};
 
 auto QORM::Utils::getDoubleOrDefault(const QSqlRecord &record,
                                      const QString &fieldName,
                                      double defaultValue) -> double {
     return getOrDefault<double>(record, fieldName, defaultValue,
-                                [](const auto &variant) -> double {
-                                  return variant.toDouble();
-                                });
+                                doubleExtractor);
+}
+
+auto QORM::Utils::getDoubleOrThrow(const QSqlRecord &record,
+                                   const QString &fieldName) -> double {
+    return getOrThrow<double>(record, fieldName, "A valid double is expected",
+                              doubleExtractor);
 }
 
 const auto validString = [](const auto &value) {
