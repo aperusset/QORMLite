da8bac99c56e5863c6ab67f5f704c45947004b43
Alexandre Perusset <alexandre.perusset@kudelskisecurity.com>
2025-05-20 13:23:25 +0200

    White list allowed characters instead of blacklist

--- test/utilstest.cpp
+++ test/utilstest.cpp
@@ -32,13 +32,13 @@ void UtilsTest::backupFileName() {
 
 void UtilsTest::parametrize() {
     // Given
-    const auto *const fieldName = "Fi'eld,Na:me.(re!ally\\-fu$nny.at%all[no?])";
+    const auto *const fieldName = "Fi'eld,Na:me09.(re!ally\\-fu$nny.at%all[no?])";
 
     // When
     const auto parametrizedFieldName = QORM::Utils::parametrize(fieldName);
 
     // Then
-    QCOMPARE(parametrizedFieldName, ":fieldnamereallyfunnyatallno");
+    QCOMPARE(parametrizedFieldName, ":fieldname09reallyfunnyatallno");
 }
 
 void UtilsTest::dateToDay() {
