930be91784249dbac9de8b982bd7c95fdd16f753
Alexandre Perusset <alexandre.perusset@kudelskisecurity.com>
2025-08-12 10:48:17 +0200

    Applied code rabbit suggestions

--- code/operations/query/selection/count.h
+++ code/operations/query/selection/count.h
@@ -9,16 +9,18 @@ namespace QORM {
 
 class Count : public Selection {
  public:
-    explicit Count(const QString &fieldToCount,
+    explicit Count(const QString &fieldToCount = Selection::ALL,
                    const std::optional<QString> &renamedTo = std::nullopt,
                    bool distinct = false) :
-        Selection(QString("count(") + (distinct ? "distinct " : "") +
-                  fieldToCount + ")", renamedTo) {
-            if (distinct && fieldToCount.simplified() == Selection::ALL) {
+        Selection([&]() -> QString {
+            const auto simplifiedCount = fieldToCount.simplified();
+            if (distinct && simplifiedCount == Selection::ALL) {
                 throw std::invalid_argument("Cannot count distinctly on " +
                                             Selection::ALL.toStdString());
             }
-        }
+            return QString("count(") + (distinct ? "distinct " : "") +
+                   simplifiedCount + ")";
+        }(), renamedTo) {}
 };
 
 }  // namespace QORM
